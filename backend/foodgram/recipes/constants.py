1bfefe1173eb23e4d6eedd77e6f7a717fe22d96f
Dmitrii <lagodnov@gmail.com>
2024-01-15 22:12:53 +0300

    models

---
+++ backend/foodgram/recipes/constants.py
@@ -0,0 +1,19 @@
+# Table Tag.
+MAX_LEN_NAME_TAG = 250
+
+MAX_LEN_COLOR_CODE_TAG = 7
+
+MAX_LEN_NAME_SLUG = 250
+
+# Table Ingredient.
+MAX_LEN_NAME_INGREDIENT = 250
+
+MAX_LEN_UNIT_INGREDIENT = 250
+
+# Table Recipe.
+MAX_LEN_NAME_RECIPE = 250
+
+UPLOAD_TO = 'recipes/',
+
+
+
--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,6 +1,16 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+from recipes.constants import (
+    MAX_LEN_NAME_TAG,
+    MAX_LEN_COLOR_CODE_TAG,
+    MAX_LEN_NAME_SLUG,
+    MAX_LEN_NAME_INGREDIENT,
+    MAX_LEN_UNIT_INGREDIENT,
+    MAX_LEN_NAME_RECIPE,
+    UPLOAD_TO
+)
+
 
 User = get_user_model()
 
@@ -9,14 +19,55 @@ class Teg(models.Model):
     """
     Модель тега.
     """
-    pass
+    name = models.CharField(
+        verbose_name='Название тега',
+        max_length=MAX_LEN_NAME_TAG,
+        unique=True,
+    )
+
+    color_code = models.CharField(
+        verbose_name='Цветовой код',
+        max_length=MAX_LEN_COLOR_CODE_TAG,
+        unique=True,
+    )
+
+    slug = models.SlugField(
+        verbose_name='Слаг',
+        max_length=MAX_LEN_NAME_SLUG,
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        default_related_name = 'tag'
+
+    def __str__(self):
+        return self.name
 
 
 class Ingredient(models.Model):
     """
     Модель ингредиента.
     """
-    pass
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=MAX_LEN_NAME_INGREDIENT,
+    )
+
+    unit = models.SlugField(
+        verbose_name='Единица измерения',
+        max_length=MAX_LEN_UNIT_INGREDIENT,
+    )
+
+    class Meta:
+        verbose_name = 'Ингридиент'
+        verbose_name_plural = 'Ингридиенты'
+        default_related_name = 'ingredient'
+        ordering = ('pk',)
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
@@ -31,12 +82,12 @@ class Recipe(models.Model):
 
     name = models.CharField(
         verbose_name='Название',
-        max_lenght=250,
+        max_length=MAX_LEN_NAME_RECIPE,
     )
 
     image = models.ImageField(
         verbose_name='Картинка',
-        upload_to='recipes/',
+        upload_to=UPLOAD_TO,
     )
 
     description = models.TextField(
@@ -50,9 +101,25 @@ class Recipe(models.Model):
 
     teg = models.ForeignKey(
         Teg,
-        verbose_name='Тег'
+        verbose_name='Тег',
+        on_delete=models.CASCADE,
+        blank=False,
     )
 
     time = models.PositiveIntegerField(
         verbose_name='Время'
     )
+
+    date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+        default_related_name = 'recipe'
+        ordering = ('-date',)
+
+    def __str__(self):
+        return self.name
