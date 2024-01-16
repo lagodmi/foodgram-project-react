13a8f331382d8b2189ed95523286f6e062a22fd9
Dmitrii <lagodnov@gmail.com>
2024-01-16 21:01:42 +0300

    admin zone

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import (
     Tag,
@@ -6,45 +7,43 @@ from .models import (
     Recipe
 )
 
+
+admin.site.site_title = 'Админ-панель сайта "FOODGRAM"'
+admin.site.site_header = 'Админ-панель сайта "FOODGRAM"'
 admin.site.empty_value_display = 'Не задано'
 
 
-# @admin.register(Teg)
-# class TegAdmin(admin.ModelAdmin):
-#     list_display = ('__all__',)
-#     list_editable = ('__all__',)
-
-
-# @admin.register(Ingredient)
-# class IngredientAdmin(admin.ModelAdmin):
-#     list_display = ('__all__',)
-#     list_editable = ('__all__',)
-
-
-# @admin.register(Recipe)
-# class RecipeAdmin(admin.ModelAdmin):
-#     list_display = ('__all__',)
-#     list_editable = ('__all__',)
-
-
-class BaseModelAdmin(admin.ModelAdmin):
-    list_display = ('__all__',)
-    list_editable = ('__all__',)
-
-    class Meta:
-        abstract = True
-
-
 @admin.register(Tag)
-class TegAdmin(BaseModelAdmin):
-    pass
+class TegAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk', 'name', 'color_code', 'slug'
+    )
+    list_editable = (
+        'name', 'color_code', 'slug'
+    )
 
 
 @admin.register(Ingredient)
-class IngredientAdmin(BaseModelAdmin):
-    pass
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk', 'name', 'unit'
+    )
+    list_editable = (
+        'name', 'unit'
+    )
 
 
 @admin.register(Recipe)
-class RecipeAdmin(BaseModelAdmin):
-    filter_horizontal = ('ingredient', 'tag')
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk', 'author', 'name', 'get_html_photo', 'description', 'time', 'date'
+    )
+    list_editable = (
+        'author', 'name', 'description', 'time'
+    )
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f'<img src="{object.photo.url}" width=50>')
+
+    get_html_photo.short_description = 'Миниатюра'
