886fac4b3842966269485081b01f838e75657538
Dmitrii <lagodnov@gmail.com>
2024-01-29 17:37:43 +0300

    beginning

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from djoser.views import UserViewSet
 from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from .serializers import (
     TagSerializer, IngredientSerializer, RecipeSerializer, UserSerializer
@@ -36,7 +37,14 @@ class UserViewSet(UserViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
-    def post(self, request, *args, **kwargs):
-        response = super().post(request, *args, **kwargs)
-        response.status_code = status.HTTP_200_OK
-        return response
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # def post(self, request, *args, **kwargs):
+        # response = super().post(request, *args, **kwargs)
+        # response.status_code = status.HTTP_200_OK
+        # return response
